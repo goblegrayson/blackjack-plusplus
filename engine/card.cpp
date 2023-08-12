968625cdb140e5c8b2560d035cfa5260f3e61444
Grayson Goble <goble.develop@gmail.com>
2023-08-11 22:24:32 -0400

    functioning hello world for the blackjack engine including: - basic cmake setup - library for the cards class (currently containing a hello world function) - testing executable

---
+++ engine/card.cpp
@@ -0,0 +1,12 @@
+﻿// card.cpp : Defines the classes for Blackjack cards.
+//
+
+#include "card.hpp"
+
+using namespace std;
+
+void card::hello(){
+	string hwStr = "Hello World!";
+	cout << hwStr << endl;
+	_sleep(5000);
+};
---
+++ engine/test/main.cpp
@@ -0,0 +1,10 @@
+// main.cpp : Run test suite.
+//
+
+#include "main.hpp"
+
+// Test Cards
+int main() {
+	card::hello();
+	return 1;
+};
